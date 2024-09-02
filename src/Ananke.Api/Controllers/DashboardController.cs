cc03f9be13353ecfd267f1944fff0bba17854734
Herwans <herwansharvel@gmail.com>
2024-09-02 23:06:25 +0200

    feat: add disks usage endpoint

---
+++ src/Ananke.Api/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using Ananke.Application.Features.Dashboard.Queries;
+using Ananke.Application.Features.Items.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ananke.Api.Controllers
+{
+    [ApiController]
+    [Route("dashboard")]
+    public class DashboardController : Controller
+    {
+        private readonly ISender _sender;
+
+        public DashboardController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet("disks-usage")]
+        public IActionResult DisksUsage()
+        {
+            return Ok(_sender.Send(new GetDisksUsageQuery()).Result);
+        }
+    }
+}
